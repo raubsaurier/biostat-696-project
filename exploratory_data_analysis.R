0c8fd00dca48313dcc23055c2f7cf857439c9cd8
Irena Chen <irena@umich.edu>
2019-03-22 14:26:48 -0400

    start on visualization code

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -26,12 +26,19 @@ airData$pollutant_count <- 1
 # just plot for one year: 
 airData2005 <- airData[Year==2005]
 airData2005$state <- tolower(as.character(airData2005$State))
+
+## let's just plot pollution totals for now and ignore what type of pollution it is: 
+graphData <- airData2005[,list(Value= sum(na.omit(Value))), by=list(stateFIPS, County, Year, state)]
+
+## load in map of US by states from the "maps" package
 states <- map_data("state")
 
-graphData <- merge(airData2005, states, by.x="State", by.y="region", allow.cartesian=TRUE)
+graphData <- merge(airData2005, states, by.x="state", by.y="region", allow.cartesian=TRUE)
 
+## plot the pollution values: 
 
-
-
+ggplot(data = states) + 
+  geom_polygon(data=graphData, aes(x = long, y = lat, fill = Value, group = group), color = "white") + 
+  coord_fixed(1.3)
 
 
