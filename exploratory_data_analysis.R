0852ce3351a2356d417a0ed882fbdae637430c51
Kaitlin Cornwell <kaitcorn@umich.edu>
2019-04-04 22:14:06 -0400

    Removed "Head" lines
    removed the strange "<<<HEAD" and "====" lines

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -4,7 +4,6 @@
 #####         
 #####
 ###############################################################################################
-# trial comment
 rm(list=ls())
 library(data.table)
 library(readxl)
@@ -16,10 +15,8 @@ library(fields)
 library(MBA)
 library(akima)
 library(ggplot2)
-<<<<<<< HEAD
-=======
 library(maps)
->>>>>>> FETCH_HEAD
+
 # set working directory to wherever the source files are: 
 #setwd("~/repos/biostat-696-project")
 
@@ -33,11 +30,8 @@ library(maps)
 airData <- data.table(read.csv("data_174749.csv"))
 airData$pollutant_count <- 1
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 # just plot for one year: 
-=======
+
 states <- map_data("state")
 # ggplot2::map_data - turn data from the maps package into a data frame suitable for plotting with ggplot2
 # states is a data frame that has lat/long coordinates of the states
@@ -51,15 +45,13 @@ airData2005$subregion <- tolower(as.character(airData2005$County))
 graphData2005 <- merge(airData2005, counties, allow.cartesian=TRUE)
 # merge 2005 data to states so it can be graphed with ggplot2
 
->>>>>>> FETCH_HEAD
+
 airData2005 <- airData[Year==2005]
 airData2005$state <- tolower(as.character(airData2005$State))
 
 ## let's just plot pollution totals for now and ignore what type of pollution it is: 
 graphData <- airData2005[,list(Value= sum(na.omit(Value))), by=list(stateFIPS, County, Year, state)]
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 # just plot for one year: 
 
 states <- map_data("state")
@@ -100,7 +92,7 @@ airData2011$subregion <- tolower(as.character(airData2011$County))
 
 graphData2011 <- merge(airData2011, counties, allow.cartesian=TRUE)
 # merge 2005 data to states so it can be graphed with ggplot2
-=======
+
 
 pdf("2005 concentrations.pdf")
 ggplot() +
@@ -135,11 +127,8 @@ ggplot() +
   geom_polygon(data = graphData2011, aes(x = long, y = lat, group = group, fill = Value)) +
   facet_wrap(~ Pollutant)
 dev.off()
-<<<<<<< HEAD
+
 # looks as if Acetaldehyde and Formaldehyde have the most variation in the data
-=======
-  # looks as if Acetaldehyde and Formaldehyde have the most variation in the data
->>>>>>> FETCH_HEAD
 
 graphData2011$logValue = ifelse(graphData2011$Value != 0, log(graphData2011$Value), log(graphData2011$Value + .0001))
 pdf("2011 logconcentrations.pdf")
@@ -148,28 +137,15 @@ ggplot() +
   geom_polygon(data = graphData2011, aes(x = long, y = lat, group = group, fill = logValue)) +
   facet_wrap(~ Pollutant)
 dev.off()
-<<<<<<< HEAD
 # on the log scale butadiene has the most variation
 
 
-=======
-## --- --- --- --- --- --- --- --- 
->>>>>>> FETCH_HEAD
-=======
-  # on the log scale butadiene has the most variation
-
-
-## --- --- --- --- --- --- --- --- 
->>>>>>> FETCH_HEAD
 #### Asthma data for Kansas by County for 2000-2017 #####
 ## --- --- --- --- --- --- --- --- 
 
 asthmaData <- data.table(read_excel("kansas_asthma_rate.xlsx"))
-<<<<<<< HEAD
 
 
-=======
->>>>>>> FETCH_HEAD
 # let's do the year 2011 and by county level 
 countyData <- asthmaData[LocationType=="County"&TimeFrame==2011]
 
@@ -181,8 +157,6 @@ get_fips <- function(state, county){
 ## get the FIPS number for each county in Kansas 
 countyData$fips <- mapply(get_fips, "Kansas", countyData$Location)
 
-=======
->>>>>>> FETCH_HEAD
 # dataset with two variables - FIPs number and the rates 
 graphData <- countyData[,c("fips", "Data")]
 graphData$Data <- as.numeric(graphData$Data)
@@ -191,17 +165,14 @@ graphData$Data <- as.numeric(graphData$Data)
 plot_usmap(include = "KY", data = graphData, values = "Data", lines = "black") + 
   scale_fill_gradient2(low="green",midpoint=median(na.omit(graphData$Data)), high="red", name="Asthma Hospitalization \n Rates") +
   theme(legend.position = "right") + 
-<<<<<<< HEAD
   labs(title="2011 Asthma Rates per 1,000 Population - Kansas County Level") 
 
 
 ## --- --- --- --- --- --- --- --- 
 #### Asthma data for Kentucky by County for 2000-2017 #####
-<<<<<<< HEAD
 
 ## this is hospitalizaton rates per 10,000 children ages 0-17 
 ## --- --- --- --- --- --- --- --- 
-=======
 
 ## this is hospitalizaton rates per 10,000 children ages 0-17 
 ## --- --- --- --- --- --- --- --- 
@@ -217,7 +188,6 @@ get_fips <- function(state, county){
 
 ## get the FIPS number for each county in KY
 countyData$fips <- mapply(get_fips, "KY", countyData$Location)
->>>>>>> FETCH_HEAD
 
 kyData <- data.table(read_xlsx("kyData.xlsx"))
 # let's do the year 2011 and by county level 
@@ -239,8 +209,6 @@ graphData$Data <- as.numeric(graphData$Data)
 plot_usmap(include = "KY", data = graphData, values = "Data", lines = "black") + 
   scale_fill_gradient2(low="green",midpoint=median(na.omit(graphData$Data)), high="red", name="Asthma Hospitalization \n Rates") +
   theme(legend.position = "right") + 
-=======
->>>>>>> FETCH_HEAD
   labs(title="2010-2012 Average Asthma Rates per 1,000 Population - KY County Level") 
 
 pdf("2011 concentrations.pdf")
