b20f9e497f45c6fd2de3440118eabc34adb5e819
Irena Chen <irena@umich.edu>
2019-04-15 12:48:10 -0400

    code for prediction
    the REML prediction one is buggy

--- spatial_analysis.R
+++ spatial_analysis.R
@@ -40,8 +40,9 @@ logistic_model = glm(cbind(asthma_count, total_population - asthma_count) ~ pct_
                      family = "binomial", data = asthma)
 summary(logistic_model)
 
-
+#### ---------------------------------------------
 ##### Use kriging on centroid lat/long coordinates: 
+#### ---------------------------------------------
 
 asthma_with_coords = data.table(read.csv("2016Asthma_Final_w_KrigingParams.csv"))
 
@@ -90,7 +91,7 @@ plot(emp.variog, main="Empirical Semi-Variogram of the Log-Linear Residuals")
 
 
 # fit an exponential variogram 
-exp.variog <- fit.variogram(emp.variog,vgm(psill=800, nugget=1500, "Lin"),fit.method=2)
+exp.variog <- fit.variogram(emp.variog,vgm(psill=800, nugget=1500, "Exp"),fit.method=2)
 exp.variog
 # make a plot of the empirical semi-variogram with the exponential semi-variogram overlayed 
 print(plot(emp.variog,exp.variog, main="Exponential Semi-Variogram"))
@@ -98,20 +99,21 @@ print(plot(emp.variog,exp.variog, main="Exponential Semi-Variogram"))
 
 ## specify the initial values for the parameters 
 beta.init <- loglinear_model$coefficients# use the estimates of beta from our loglinear GLM model
-sigma2.init <- exp.variog$psill[2] # partial sill 
+sigma2.init <- exp.variog$psill[2] + 0.0001 # since this is 0, add a very small value so that the REML model can run 
 phi.init <- 1/exp.variog$range[2] # range 
 tau2.init <- exp.variog$psill[1]  #we want to include a nugget effect
-coords <- as.matrix(cbind(x.dist, y.dist)) ##easting and northing coordinates from the data 
 
 #### ---------------------------------------------
 ### Code for frequentist model w/ spatial effects: 
 #### ---------------------------------------------
 
 
-asthma_subset <- asthma_with_coords[,c("asthma_count", "obesity_rate_2016", 
-                                      "pct_daily_smokers", "meanAQI.Ozone", "meanAQI.Other", "pct_black", "x.dist", "y.dist")]
+asthma_model <- asthma_with_coords[,c("total_population","asthma_count", "obesity_rate_2016", 
+                                      "pct_daily_smokers", "meanAQI.Ozone", "meanAQI.Other", "pct_black", "lat", "long")]
 
-asthma_subset <- asthma_subset[!is.na(asthma_subset$asthma_count)&!is.na(asthma_subset$pct_black)]
+asthma_subset <- asthma_model[!is.na(asthma_model$asthma_count)&!is.na(asthma_model$pct_black)]
+coords <- as.matrix(cbind(asthma_subset$lat, asthma_subset$long)) 
+
 
 ## get the covariates 
 asthma_count <- asthma_subset$asthma_count
@@ -120,9 +122,10 @@ pct_daily_smokers <- asthma_subset$pct_daily_smokers
 meanAQI.Ozone <- asthma_subset$meanAQI.Ozone
 meanAQI.Other <- asthma_subset$meanAQI.Other
 pct_black <- asthma_subset$pct_black
+total_population <- asthma_subset$total_population
 
 
-br.geo <- as.geodata(asthma_subset,coords.col=c(7,8), data.col = 1, covar.col = c(2:6))
+br.geo <- as.geodata(asthma_subset,coords.col=c(8,9), data.col = 2, covar.col = c(3,4,5,6,7))
 
 ## fit the model 
 set.seed(04122019)
@@ -132,12 +135,45 @@ br.reml
 
 
 #### ---------------------------------------------
+##### making predictions with the data 
+#### ---------------------------------------------
+# first we will use the above model to make predictions on the last 20 observations
+# get the last 20 observations of the BR price data 
+asthma_pred <- asthma_model[is.na(asthma_model$asthma_count)&!is.na(asthma_model$pct_black)]
+
+# the locations of the observations we want to predict on 
+lat0 <- asthma_pred$lat
+long0 <-asthma_pred$long
+
+pred_coords <- as.matrix(cbind(lat0, long0))
+
+## get the estimates of phi, sigma, tau from the REML 
+sigma2.pred <- br.reml$sigmasq
+phi.pred <-br.reml$phi
+tau2.pred <- br.reml$tausq
+
+
+# set up the model for prediction 
+set.seed(03012019)
+kc.ok.control <- krige.control(type.krige="ok",trend.d = obesity_rate_2016 + pct_daily_smokers + meanAQI.Ozone + meanAQI.Other + pct_black
+                               , obj.model = br.reml,
+                              trend.l =  asthma_pred$obesity_rate_2016 + asthma_pred$pct_daily_smokers + asthma_pred$meanAQI.Ozone + asthma_pred$meanAQI.Other +asthma_pred$pct_black,
+                              cov.model="exponential", 
+                              cov.pars=c(sigma2.pred,phi.pred),nugget=tau2.pred)
+
+
+loc.ok <- matrix(c(lat0,long0),nrow=18,ncol=2)
+kc.ok.s0 <- krige.conv(br.geo,locations=loc.ok,krige=kc.ok.control)
+
+pred_prices <- kc.ok.s0$predict
+
+#### ---------------------------------------------
 ##### Bayesian hierarchical model w/ spatial effects: 
 #### ---------------------------------------------
 
 
 ## set up the model 
-n.batch <- 1000
+n.batch <- 2000
 batch.length <- 100
 n.samples = n.batch*batch.length
 burn.in <- 0.5*n.samples
@@ -145,15 +181,18 @@ burn.in <- 0.5*n.samples
 # for reproducibility
 set.seed(20190301)
 ## fit a Bayesian GLM 
-nyBayes <- spGLM(ny_data$log_cases ~  ny_data$Pct.homeowners + ny_data$Pct.over65 + ny_data$Avg.inv.dist.TCEs, weights = ny_data$Population.1980, family="poisson", 
+## not sure if I set the weights correctly, feel free to change!! 
+asthmaBayes <- spGLM(asthma_count ~obesity_rate_2016 + pct_daily_smokers + meanAQI.Ozone + meanAQI.Other + pct_black
+                 , weights = total_population, family="poisson", 
                  coords=coords,starting=list("phi"=phi.init,"sigma.sq"=sigma2.init, "tau.sq"=tau2.init,"beta"=beta.init, "w"=0),
-                 tuning=list("phi"=0.0075, "sigma.sq"=0.005, "tau.sq"=0.001, "beta"=c(0.02, 0.02, 0.02, 0.02), "w"=0.0001), #tried several different values of the tuning to make the trace plots look nicer 
-                 priors=list("phi.Unif"=c(0.03, 0.9), "sigma.sq.IG"=c(3, 1.5),
-                             "tau.sq.IG"=c(3, 1),"beta.Flat"), cov.model="exponential",n.samples=n.samples, verbose=TRUE, n.report=0.2*n.samples)
+                 tuning=list("phi"=0.000001, "sigma.sq"=0.00005, "tau.sq"=0.0001, beta=c(rep(0.00001, length(beta.init))), "w"=0.0001), #tried several different values of the tuning to make the trace plots look nicer 
+                 priors=list("phi.Unif"=c(1, 10), "sigma.sq.IG"=c(2, 1),
+                             "tau.sq.IG"=c(3, 1),"beta.Flat"), cov.model="gaussian",n.samples=n.samples, verbose=TRUE, n.report=0.2*n.samples)
 
 ## Trace plots of the parameters
 par(mai=rep(0.5,4))
-plot(nyBayes$p.beta.theta.samples)
+plot(asthmaBayes$p.beta.theta.samples)
+
 
 
 
